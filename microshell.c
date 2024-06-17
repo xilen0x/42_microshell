9238e3c287084b679006c569e6e69089e11600ad
Carlos Astorga <castorga@car15s3.42barcelona.com>
2024-06-17 17:24:11 +0200

    ended

--- microshell.c
+++ microshell.c
@@ -1,8 +1,6 @@
-#include <unistd.h>
-#include <string.h>
-#include <sys/wait.h>
+#include "microshell.h"
 
-err(char *str)
+int	put_std_err(char *str)
 {
 	while (*str)
 		write (2, str++, 1);
@@ -12,56 +10,43 @@ err(char *str)
 int	cd(int ac, char **av)
 {
 	if (ac != 2)
-		return (err("error: cd: bad arguments\n"));
+		return (put_std_err("error: cd: bad arguments\n"));
 	if (chdir(av[1]) == -1)
-		return (err("error: cd: cannot change directory to"), err(av[1]), err("\n"));
+		return (put_std_err("error: cd: cannot change directory to "), put_std_err(av[1]), put_std_err("\n"));
 	return (0);
 }
-// Función para ejecutar comandos
-int	exec(int i, char **av, char **envp)
+int executor(int i, char **av, char **envp)
 {
-	int fd[2];           // Array para los descriptores de archivo de la tubería
-	int status;          // Variable para almacenar el estado del proceso hijo
-	int has_pipe = av[i] && !strcmp(av[i], "|");  // Verifica si el comando actual tiene una tubería
-
-	// Si no hay tubería y el comando es 'cd', llama a la función cd
-	if (!has_pipe && !strcmp(*av, "cd"))
-		return cd(av, i);
-	// Si hay tubería, crea la tubería
-	if (has_pipe && pipe(fd) == -1)
-		return err("error: fatal\n");
-
-	// Crea un proceso hijo
-	int pid = fork();
-	if (!pid)
+	int	has_pipe;
+	int	fd[2];
+	int pid;
+	int status;
+	
+	has_pipe = av[i] && !strcmp(av[i], "|");
+	if(!has_pipe && !strcmp(*av, "cd"))
+		return (cd(i, av));
+	if(has_pipe && pipe(fd) == -1)
+		return (put_std_err("error: fatal\n"));
+	pid = fork();
+	if(!pid)
 	{
-        av[i] = 0;  // Termina la lista de argumentos en el índice 'i' poniendo NULL
-        // Si hay tubería, redirige la salida estándar al extremo de escritura de la tubería
-        if (has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-            return err("error: fatal\n");
-        // Si el comando es 'cd', llama a la función cd
-        if (!strcmp(*av, "cd"))
-            return cd(av, i);
-        // Ejecuta el comando especificado en av[0] con sus argumentos y el entorno envp
-        execve(*av, av, envp);
-        // Si execve falla, imprime un mensaje de error y el comando que no se pudo ejecutar
-        return err("error: cannot execute "), err(*av), err("\n");
-    }
-	// Espera a que el proceso hijo termine
+		av[i] = 0;
+		if(has_pipe && (dup2(fd[1], STDOUT_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+			return (put_std_err("error: fatal\n"));
+		if(!strcmp(*av, "cd"))
+			return (cd(i, av));
+		execve(*av, av, envp);
+		return (put_std_err("error: cannot execute "), put_std_err(*av), put_std_err("\n"));
+	}
 	waitpid(pid, &status, 0);
-
-	// Si hay tubería, redirige la entrada estándar al extremo de lectura de la tubería
-	if (has_pipe && (dup2(fd[0], 0) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
-		return err("error: fatal\n");
-	// Devuelve el estado del proceso hijo
+	if(has_pipe && (dup2(fd[0], STDIN_FILENO) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
+		return (put_std_err("error: fatal\n"));
 	return (WIFEXITED(status) && WEXITSTATUS(status));
 }
-//int	executor(int ac, char **av, char **envp);
-
 int	main(int ac, char **av, char **envp)
 {
-	int	status = 0;
-	int	i = 0;
+	int status = 0;
+	int i = 0;
 
 	if (ac > 1)
 	{
@@ -72,7 +57,7 @@ int	main(int ac, char **av, char **envp)
 			while (av[i] && (strcmp(av[i], "|") && strcmp(av[i], ";")))
 				i++;
 			if (i)
-				status = executor(i, av, envp);
+				status = executor(i, av, envp);			
 		}
 	}
 	return (status);
